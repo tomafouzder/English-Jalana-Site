32c9f501cfe74648f7489bcbcf8d562234353d92
tomafouzder <tomafouzder12@gmail.com>
2025-09-13 19:28:45 +0600

    added this box container with js code

--- script/index.js
+++ script/index.js
@@ -3,7 +3,48 @@ const loadLessons = () => {
         .then(res => res.json()) //promise of json data
         .then(json => displayLesson(json.data));
 };
+//.......... lesson word box container section start here..........//
+const loadLevelWord = (id) => {
+    const url = `https://openapi.programming-hero.com/api/level/${id}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayLevelWord(data.data));
+};
 
+const displayLevelWord = (words) => {
+    const wordContainer = document.getElementById('word-container');
+    wordContainer.innerHTML = "";
+
+//     {
+//     "id": 82,
+//     "level": 1,
+//     "word": "Car",
+//     "meaning": "গাড়ি",
+//     "pronunciation": "কার"
+// }
+
+    words.forEach((word) => {
+        console.log(word);
+        const card = document.createElement("div");
+        card.innerHTML = `
+        <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
+
+            <h2 class="font-bold text-2xl">${word.word}</h2>
+            <p class="font-semibold">Meaning /Pronounciation</p>
+            <div class="font-medium text-xl font-bangla">"${word.meaning} / ${word.pronunciation}"</div>
+            <div class="flex justify-between items-center">
+                <button class="btn bg-[#1a91ff1a] hover:bg-[#1a91ff85]"><i class="fa-solid fa-circle-info"></i></button>
+                <button class="btn bg-[#1a91ff1a] hover:bg-[#1a91ff85]"><i class="fa-solid fa-volume-high"></i></button>
+            </div>
+
+        </div>
+        `;
+        wordContainer.append(card);
+    });
+};
+
+
+//.......... lesson button section start here..........//
 const displayLesson = (lessons) => {
     // 1.get the container & empty
     const levelContainer = document.getElementById('level-container');
@@ -11,12 +52,13 @@ const displayLesson = (lessons) => {
 
     // 2. get into every lessons
     for (let lesson of lessons) {
-        console.log(lesson);
+        // console.log(lesson);
         //         3. create Element
         const btnDiv = document.createElement("div");
-        btnDiv.innerHTML = ` <button class="btn btn-outline btn-primary">
-                            <i class="fa-solid fa-book-open"></i>
-                            Lesson - ${lesson.level_no}</button>
+        btnDiv.innerHTML = ` 
+        <button onclick = "loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary">
+        <i class="fa-solid fa-book-open"></i>
+        Lesson - ${lesson.level_no}</button>
 `;
 
         //         4. append into container
